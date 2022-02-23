9b708bae9f52773334b668f413dfbd1c19caf014
Istarengwa <muller.lilian@outlook.fr>
2022-02-23 11:23:03 +0100

    add test for all test in one

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -7,6 +7,32 @@ describe("Gilded Rose", function() {
     listItems = [];
   });
 
+  it("test complet", () => {
+    const items = [
+      new Item("+5 Dexterity Vest", 10, 20),
+      new Item("Aged Brie", 2, 0),
+      new Item("Elixir of the Mongoose", 5, 7),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 39),
+    
+      // This Conjured item does not work properly yet
+      new Item("Conjured Mana Cake", 3, 6),
+    ];
+    
+    const days = 5;
+    const gildedRose = new Shop(items);
+
+    for (let day = 0; day < days; day++) {
+      console.log(`\n-------- day ${day} --------`);
+      console.log("name, sellIn, quality");
+      items.forEach(item => console.log(`${item.name}, ${item.sellIn}, ${item.quality}`));
+      gildedRose.updateQuality();
+    }
+  });
+
   it("Baisser de 1 la qualité et la date de péremption d'item normaux", () => {
     listItems.push(new Item('+5 Dexterity Vest', 10, 20));
     listItems.push(new Item('Elixir of the Mongoose',  5, 7));
